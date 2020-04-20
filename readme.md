ccfd65a7debd55554af3d3654d34588f6c39b6ce
Steve <stevematos1998>
2020-04-20 12:25:37 -0500

    [DOCS] actualizando git ignore

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Proyecto Pugstagram
+
+Proyecto de [svelte](https://platzi.com/cursos/svelte/) de platzi.
---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<style>
+    .Footer {
+        font-size: 11px;
+        letter-spacing: 1px;
+        font-weight: normal;
+        color: #c7c7c7;
+    }
+</style>
+
+<div class="Footer">
+    <div class="Footer-container">
+        <div class="footer-copy">
+            © 2020 PUGSTAGRAM FROM Steve
+        </div>
+    </div>
+</div>
